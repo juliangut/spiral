be97b9903f3c48591d5562342a1d9a51b2fa5125
Julián Gutiérrez <juliangut@gmail.com>
2016-02-10 13:01:29 +0100

    styleci fixes on tests

--- tests/Spiral/Option/OptionRegexTest.php
+++ tests/Spiral/Option/OptionRegexTest.php
@@ -3,6 +3,7 @@
  * Spiral: PSR7 aware cURL client (https://github.com/juliangut/spiral)
  *
  * @link https://github.com/juliangut/spiral for the canonical source repository
+ *
  * @license https://raw.githubusercontent.com/juliangut/spiral/master/LICENSE
  */
 
